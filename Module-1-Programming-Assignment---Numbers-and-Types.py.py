69d64a7361748ef9f0a620b43e0ee3b3c41f95e8
AngelClasswork <151481591+AngelClasswork@users.noreply.github.com>
2024-09-18 17:49:24 -0500

    Add files via upload

---
+++ Module-1-Programming-Assignment---Numbers-and-Types.py.py
@@ -0,0 +1,7 @@
+seconds_per_minute = 60
+seconds_per_hour = seconds_per_minute * 60
+seconds_per_day = seconds_per_hour * 24
+
+print("For floating point division the result is:", seconds_per_day / seconds_per_hour)
+
+print("For integer division the result is:", seconds_per_day // seconds_per_hour)
